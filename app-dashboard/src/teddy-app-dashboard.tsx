c70bf0afd8794b494de50f3f9e7756ba8896cede
SaraGraminho <saragraminho230@gmail.com>
2024-01-06 13:05:20 -0300

    feat: removendo o tailwind do app dashboard

--- app-dashboard/src/teddy-app-dashboard.tsx
+++ app-dashboard/src/teddy-app-dashboard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
 import Root from "./root.component";
-import "./styles.css";
+import "./styles/globals.css";
 
 const lifecycles = singleSpaReact({
   React,
